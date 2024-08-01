32735456aae5eecac7113da1ebc95a6fa7de259d
priest-2105 <fawzybailey782@gmail.com>
2024-08-01 16:47:22 +0100

    ✨✨commit

--- src/components/common/Preloader.jsx
+++ src/components/common/Preloader.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import logoWordmark from '../../../static/logo-wordmark.png';
+import gif from '../../../static/preloader.gif';
 
 const Preloader = () => (
   <div className="preloader">
-    <svg className="logo-symbol" viewBox="0 0 41.25 41.25">
+    {/* <svg className="logo-symbol" viewBox="0 0 41.25 41.25">
       <circle cx="20.62" cy="20.62" r="20.62" />
       <circle className="fill-white" cx="29.97" cy="14.93" r="6.58" />
-    </svg>
-    <img alt="Xvision logo wordmark" src={logoWordmark} />
+    </svg> */}
+    <img alt="Xvision logo wordmark" src={gif} />
   </div>
 );
 
